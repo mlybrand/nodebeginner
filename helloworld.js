0fa20d50389bc0ea242b5f37fd98fe0f2ece249d
Mark Lybrand <mlybrand@gmail.com>
2012-04-03 20:44:24 -0700

    finished through how function passing makes our HTTP server work

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+function say(word) {
+  console.log(word);
+}
+
+function execute(someFunction, value) {
+  someFunction(value);
+}
+
+execute(say, "Hello");
+
+execute(function(word) { console.log(word); }, "Hello");
