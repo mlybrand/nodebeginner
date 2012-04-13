a547faa4e90c04743680a479d035b3a8a46de98f
Mark Lybrand <mlybrand@gmail.com>
2012-04-13 15:00:03 -0700

    finished up the node beginner book and this is the final code for that lesson

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,62 @@
-function start() {
+var querystring = require("querystring"),
+    fs = require("fs"),
+    sys = require("util");
+    formidable = require("formidable");
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" enctype="multipart/form-data" ' +
+    'method="post">' +
+    '<input type="file" name="upload" multiple="multiple" />' +
+    '<input type="submit" value="Upload file" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+  // read Mixu's post on understanding the node.js event loop!!!
 }
 
-function upload() {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(err, fields, files) {
+    console.log("parsing done");
+    console.log(sys.inspect({fields: fields, files: files}));
+    fs.renameSync(files.upload.path, "/tmp/test.png");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image: <br />");
+    response.write("<img src='/show' />");
+    response.end();
+  })
+}
+
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if (error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
